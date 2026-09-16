3612f9711db849321ad21b2392bdea6478d9ec9e
pkorba <32198274+pkorba@users.noreply.github.com>
2026-09-17 01:21:53 +0200

    Add support for editing the result by reacting with emoji

--- animanga/animanga.py
+++ animanga/animanga.py
@@ -1,15 +1,18 @@
 import io
 import mimetypes
+import re
 from typing import Any, Tuple, Type
 
 from PIL import Image, UnidentifiedImageError
 from aiohttp import ClientTimeout, ClientError
 from mautrix.errors import MatrixResponseError
+from mautrix.types import EventType, ReactionEvent, BaseRoomEvent
 from mautrix.util.config import BaseProxyConfig, ConfigUpdateHelper
 from maubot import Plugin, MessageEvent
 from maubot.handlers import command
 
 from .resources import queries
+from .resources.datastructures import result_indexes
 from .resources.parsers import Parser
 from .resources.formatters import Formatter
 
@@ -22,6 +25,9 @@ class Config(BaseProxyConfig):
 
 
 class AniMangaBot(Plugin):
+    OTHERS = re.compile(r"<b>Other results:</b>(.*?)</div>", re.DOTALL)
+    ANILIST_URL = re.compile(r"https://anilist\.co/(?:anime|manga)/(\d+)")
+    MYANIMELIST_URL = re.compile(r"https://myanimelist\.net/(?:anime|manga)/(\d+)")
     al_url = "https://graphql.anilist.co"
     mal_url = "https://api.tenrai.org/v1"
     headers = {
@@ -77,6 +83,123 @@ class AniMangaBot(Plugin):
             await self._mal_message_handler(evt, title, "MANGA")
         else:
             await self._al_message_handler(evt, title, "MANGA")
+
+    @command.passive(
+        regex="1️⃣|2️⃣|3️⃣|4️⃣|5️⃣|6️⃣|7️⃣|8️⃣|9️⃣|👎️|",
+        field=lambda evt: evt.content.relates_to.key,
+        event_type=EventType.REACTION,
+        msgtypes=[]
+    )
+    async def edit(self, evt: ReactionEvent, key: Tuple[str]) -> None:
+        """
+        Remove or edit the existing result.
+        :param evt: message event
+        """
+        bot_message_id = evt.content.relates_to.event_id
+        bot_message = await self.client.get_event(room_id=evt.room_id, event_id=bot_message_id)
+
+        if bot_message.sender != self.client.mxid:
+            return
+
+        user_message_id = bot_message.content.get_reply_to()
+        if user_message_id is None:
+            # Bot's message was not a reply, so not a result of a command issued to this plugin
+            return
+
+        # Inspect user message that contains the command
+        user_message = await self.client.get_event(room_id=evt.room_id, event_id=user_message_id)
+        if (
+                user_message.sender != evt.sender or
+                not user_message.content.body.startswith(("!anime", "!manga"))
+        ):
+            # Author of reaction is either not the author of the command
+            # or the reaction doesn't concern the message sent by this plugin
+            return
+
+        # Check if the bot message has been edited. Edited message won't have 'Other results'
+        # section, so only allow deleting the message
+        if (
+                evt.content.relates_to.key != "👎️" and
+                bot_message.unsigned and
+                "m.relations" in bot_message.unsigned
+        ):
+            if "m.replace" in bot_message.unsigned["m.relations"]:
+                return
+
+        await self._try_edit(evt, bot_message, user_message)
+
+    async def _try_edit(
+            self,
+            evt: BaseRoomEvent,
+            bot_message: MessageEvent,
+            user_message: MessageEvent
+    ) -> None:
+        """
+        Remove or edit the message
+        :param evt: event initiating the process
+        :param bot_message: message to be deleted/edited
+        :param user_message: message bot replied to
+        """
+        # User requested message to be redacted
+        if evt.content.relates_to.key == "👎️":
+            await self.client.redact(
+                room_id=evt.room_id,
+                event_id=bot_message.event_id,
+                reason=f"Wrong result redacted by {evt.sender}"
+            )
+            return
+
+        # User requested to change the result
+        edit_id = result_indexes[evt.content.relates_to.key]
+        media_ids = await self._get_other_media_ids(bot_message.content.formatted_body)
+        if edit_id >= len(media_ids):
+            return
+
+        # Get detailed information about the entry chosen by the user
+        media_type = "ANIME" if user_message.content.body.startswith("!anime") else "MANGA"
+        query = queries.anime if media_type == "ANIME" else queries.manga
+        try:
+            if self.config["use_mal_api"]:
+                main_result_json = await self._mal_get_main_result(media_ids[edit_id], media_type)
+            else:
+                json = {
+                    "query": query,
+                    "variables": {
+                        "id": media_ids[edit_id]
+                    }
+                }
+                main_result_json = await self._al_get_results(json)
+        except ClientError as e:
+            self.log.error(f"Failed to get data for edit: {e}")
+            return
+
+        if self.config["use_mal_api"]:
+            main_result = await self.pr.mal_parse_main_result(main_result_json)
+        else:
+            main_result = await self.pr.al_parse_main_result(main_result_json)
+        if not main_result:
+            self.log.error("Failed to parse data for the edit.")
+            return
+
+        if main_result.image:
+            main_result.image = await self._get_matrix_image_url(main_result.image)
+
+        content = await self.fmt.prepare_message(main_result, [])
+        if content:
+            await bot_message.edit(content)
+        else:
+            self.log.error("Error during preparation of the edit summary.")
+
+    async def _get_other_media_ids(self, body: str) -> list[int]:
+        other_results_section = self.OTHERS.search(body)
+        media_ids = []
+        if other_results_section:
+            other_results = other_results_section.group(1)
+            if self.config["use_mal_api"]:
+                media_ids = self.MYANIMELIST_URL.findall(other_results)
+            else:
+                media_ids = self.ANILIST_URL.findall(other_results)
+        return media_ids
 
     async def _al_message_handler(self, evt: MessageEvent, title: str, media_type: str) -> None:
         """
